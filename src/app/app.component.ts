ea5876a31df0a03d8fea219752011fd9fe551c6f
Giovanni230201 <ilgenius234@gmail.com>
2023-03-22 21:24:10 +0100

    creato carousel con aggiunta foto che sono ancora da modificare

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'abbigliamento';
+  title = 'cibando';
+
+  images=[
+    {id:1,
+    label:'Abbigliamento uomo'},
+   {id:2,
+      label:'Abbigliamento donna'},
+      {id:3,
+        label:'Abbigliamento bambini'}
+  ];
+
+  percorso="../assets/images/abbigliamento-";
 }
